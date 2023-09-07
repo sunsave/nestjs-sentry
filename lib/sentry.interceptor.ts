8e33eb1fcd48f04d35021191cd2b2d24f2b136b5
Danielle Huntrods <852397+dkhuntrods@users.noreply.github.com>
2023-09-07 12:38:02 +0100

    improve filtering clarity

--- lib/sentry.interceptor.ts
+++ lib/sentry.interceptor.ts
@@ -4,13 +4,13 @@ import {
   ExecutionContext,
   HttpException,
   Injectable,
-  NestInterceptor
+  NestInterceptor,
 } from '@nestjs/common';
-import { 
+import {
   HttpArgumentsHost,
   WsArgumentsHost,
   RpcArgumentsHost,
-  ContextType
+  ContextType,
 } from '@nestjs/common/interfaces';
 // Rxjs imports
 import { Observable } from 'rxjs';
@@ -22,77 +22,57 @@ import { Handlers } from '@sentry/node';
 import { SentryService } from './sentry.service';
 import { SentryInterceptorOptions, SentryInterceptorOptionsFilter } from './sentry.interfaces';
 
-
 @Injectable()
 export class SentryInterceptor implements NestInterceptor {
-
-  protected readonly client: SentryService = SentryService.SentryServiceInstance()
-  constructor(
-    private readonly options?: SentryInterceptorOptions
-  ) {}
+  protected readonly client: SentryService = SentryService.SentryServiceInstance();
+  constructor(private readonly options?: SentryInterceptorOptions) {}
 
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
     // first param would be for events, second is for errors
     return next.handle().pipe(
-      tap(null, (exception : HttpException) => {
-        if(this.shouldReport(exception)) {
+      tap(null, (exception: HttpException) => {
+        if (this.shouldReport(exception)) {
           this.client.instance().withScope((scope) => {
             return this.captureException(context, scope, exception);
-          })
+          });
         }
-      })
+      }),
     );
   }
 
   protected captureException(context: ExecutionContext, scope: Scope, exception: HttpException) {
     switch (context.getType<ContextType>()) {
       case 'http':
-        return this.captureHttpException(
-          scope, 
-          context.switchToHttp(), 
-          exception
-        );
+        return this.captureHttpException(scope, context.switchToHttp(), exception);
       case 'rpc':
-        return this.captureRpcException(
-          scope,
-          context.switchToRpc(),
-          exception,
-        );
+        return this.captureRpcException(scope, context.switchToRpc(), exception);
       case 'ws':
-        return this.captureWsException(
-          scope,
-          context.switchToWs(),
-          exception,
-        );
+        return this.captureWsException(scope, context.switchToWs(), exception);
     }
   }
 
-  private captureHttpException(scope: Scope, http: HttpArgumentsHost, exception: HttpException): void {
-    const data = Handlers.parseRequest(<any>{},http.getRequest(), this.options);
+  private captureHttpException(
+    scope: Scope,
+    http: HttpArgumentsHost,
+    exception: HttpException,
+  ): void {
+    const data = Handlers.parseRequest(<any>{}, http.getRequest(), this.options);
 
     scope.setExtra('req', data.request);
-    
+
     if (data.extra) scope.setExtras(data.extra);
     if (data.user) scope.setUser(data.user);
 
     this.client.instance().captureException(exception);
   }
 
-  private captureRpcException(
-    scope: Scope,
-    rpc: RpcArgumentsHost,
-    exception: any,
-  ): void {
+  private captureRpcException(scope: Scope, rpc: RpcArgumentsHost, exception: any): void {
     scope.setExtra('rpc_data', rpc.getData());
 
     this.client.instance().captureException(exception);
   }
 
-  private captureWsException(
-    scope: Scope,
-    ws: WsArgumentsHost,
-    exception: any,
-  ): void {
+  private captureWsException(scope: Scope, ws: WsArgumentsHost, exception: any): void {
     scope.setExtra('ws_client', ws.getClient());
     scope.setExtra('ws_data', ws.getData());
 
@@ -104,15 +84,17 @@ export class SentryInterceptor implements NestInterceptor {
 
     // If any filter passes, then we do not report
     if (this.options) {
-      const opts: SentryInterceptorOptions = this.options as object
+      const opts: SentryInterceptorOptions = this.options as object;
       if (opts.filters) {
-        const filters: SentryInterceptorOptionsFilter[] = opts.filters
-        return filters.some(({ type, filter }) => {
-          if (exception instanceof type) {
-            return filter === undefined ? false : !filter(exception)
-          } 
-          return false         
+        const exclusionFilters: SentryInterceptorOptionsFilter[] = opts.filters;
+
+        const hasApplicableExclusions = exclusionFilters.some(({ type, filter }) => {
+          const isInstance = exception instanceof type;
+          const hasFilter = filter !== undefined;
+          return (isInstance && !hasFilter) || (isInstance && hasFilter && filter(exception));
         });
+
+        return hasApplicableExclusions === false;
       }
     } else {
       return true;
