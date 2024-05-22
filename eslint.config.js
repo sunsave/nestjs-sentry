6440d28e2034669b1b840509da5ea816dcf7d420
Danielle Huntrods <852397+dkhuntrods@users.noreply.github.com>
2024-05-22 13:43:52 +0100

    update eslint

---
+++ eslint.config.js
@@ -0,0 +1,32 @@
+// @ts-check
+
+const eslint = require('@eslint/js');
+const tseslint = require('typescript-eslint');
+const globals = require('globals');
+
+module.exports = tseslint.config(
+  {
+    ignores: ['dist', 'eslint.config.js'],
+  },
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    languageOptions: {
+      parserOptions: {
+        project: true,
+        tsconfigRootDir: __dirname,
+      },
+      globals: {
+        ...globals.node,
+        ...globals.jest,
+      },
+    },
+
+    rules: {
+      '@typescript-eslint/interface-name-prefix': 'off',
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+    },
+  },
+);
